0df807b51e0b580b717deba483dfb764579f766e
Razan Muhammad <razanmfs507@gmail.com>
2024-02-27 08:02:58 +0700

    setup

---
+++ src/views/MainPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <Drawer />
+  <div>hello guys</div>
+</template>
+
+<script>
+import Drawer from "../components/Drawer.vue";
+export default {
+  components: { Drawer },
+};
+</script>
+
+<style></style>
