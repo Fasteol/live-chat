b1b3da0a7c8bd6270c66a10d8cd5265ae884408b
Harmoni Natanael S <harmoninatanaels@gmail.com>
2024-03-04 09:41:55 +0700

    Add Backend code

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,13 +1,20 @@
 <template>
   <Drawer />
   <div>hello guys</div>
+  <input type="text" v-model="inputMessage">
+  <button @click="sendMessage"></button>
 </template>
 
 <script>
 import Drawer from "../components/Drawer.vue";
 export default {
   components: { Drawer },
+  methods: {
+    sendMessage(){
+    }
+  }
 };
 </script>
+
 
 <style></style>
